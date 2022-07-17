14429fb8ee39793b0eab9b0a9e107967df39791b
Swarnit Sinha <swarnsinha786@gmail.com>
2022-07-17 10:49:36 +0530

    Create longest palindromic subsequence.java

---
+++ Dynamic Programming/String/longest palindromic subsequence.java
@@ -0,0 +1,30 @@
+Given a string s, find the longest palindromic subsequence's length in s.
+
+A subsequence is a sequence that can be derived from another sequence by deleting some or no elements without changing the order of the remaining elements.
+
+ 
+
+Example 1:
+
+Input: s = "bbbab"
+Output: 4
+Explanation: One possible longest palindromic subsequence is "bbbb".
+  
+  
+Example 2:
+
+Input: s = "cbbd"
+Output: 2
+Explanation: One possible longest palindromic subsequence is "bb".
+
+
+  "LOGIC IN THIS QUESTION IS"
+  
+  
+  THINK ABOUT - ' IF WE REVERSE THE GIVEN STRING AND FIND' LONGEST COMMON SUBSEQUENCE ' THEN WE WILL GET THE ANSWER '
+  THINK ABOUT - ' IF WE REVERSE THE GIVEN STRING AND FIND' LONGEST COMMON SUBSEQUENCE ' THEN WE WILL GET THE ANSWER '
+  THINK ABOUT - ' IF WE REVERSE THE GIVEN STRING AND FIND' LONGEST COMMON SUBSEQUENCE ' THEN WE WILL GET THE ANSWER '
+  
+  
+    'M' Y N 'A' M E I 'S S A M' R A T                 -> MASSAM
+    T A R 'M A S S' I E M 'A' N Y 'M'                 -> MASSAM
