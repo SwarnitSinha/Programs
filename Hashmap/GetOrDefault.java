3b41a19babf1098c7379d75533116ca3c2203a68
Swarnit Sinha <swarnsinha786@gmail.com>
2022-10-07 17:01:26 +0530

    Update GetOrDefault.java

--- Hashmap/GetOrDefault.java
+++ Hashmap/GetOrDefault.java
@@ -8,3 +8,22 @@ class Solution {
   }
   
 }
+WORKS IN TREEMAP AS WELL
+
+if(map.containsKey(start)){
+    map.put(start,map.get(start)+1);
+}					   	"OR"      map.put(start, map.getOrDefault(start,0)+1);
+else{
+    map.put(start,1);
+}
+
+
+
+if(map.containsKey(end)){
+    map.put(end,map.get(end)-1);
+}						"OR"	map.put(end, map.getOrDefault(end,0)-1);
+else{
+    map.put(end,-1);
+}
+       
+        
